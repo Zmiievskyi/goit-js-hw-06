9bb597fa6e59a4115097c7cdb7facc3b7f399ad8
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-09-24 17:02:14 +0300

    update

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,22 @@
 const inpRef = document.querySelector("#validation-input");
-console.dir(inpRef);
+const limit = Number(inpRef.getAttribute("data-length"));
 inpRef.addEventListener("blur", (event) => {
-  inpRef.classList.add("invalid")
-  if (event.currentTarget.value.length === inpRef.nextSibling.length) {
-     inpRef.classList.toggle("invalid")
-     inpRef.classList.add("valid")}
-  });
+  replaceClassList("invalid", "valid");
+
+  if (inpRef.value.length === limit) {
+    replaceClassList("valid", "invalid");
+  }
+  if (inpRef.value.length === 0) {
+    removeAllClass("valid", "invalid");
+  }
+});
+
+const replaceClassList = (add, remove) => {
+  inpRef.classList.add(add);
+  inpRef.classList.remove(remove);
+};
+
+const removeAllClass = (one, two) => {
+  inpRef.classList.remove(one);
+  inpRef.classList.remove(two);
+};
--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const getRandomHexColor = () => {
+  //const randomColor = Math.round(Math.random(0, 1) * 255);
+
+  const red = Math.round(Math.random(0, 1) * 255);
+  const green = Math.round(Math.random(0, 1) * 255);
+  const blue = Math.round(Math.random(0, 1) * 255);
+
+  return `rgb(${red},${green},${blue})`;
+};
 
 const refs = {
   input: document.querySelector("input"),
@@ -12,20 +22,32 @@ const refs = {
 refs.btnCreate.addEventListener("click", createBoxes);
 refs.btnDestroy.addEventListener("click", destroyBoxes);
 
-function createBoxes() {
-  const amount = refs.input.value;
+function createBoxes(amount) {
+  amount = refs.input.value;
+  const divBox = [];
   for (let i = 0, size = 30; i < amount; i += 1, size += 10) {
-    // refs.container.style.display = 'flex';
-    // refs.container.style.flexWrap = "wrap";
     const divEl = document.createElement("div");
     divEl.style.width = size + "px";
     divEl.style.height = size + "px";
     divEl.style.backgroundColor = getRandomHexColor();
-    refs.container.append(divEl);
-     }
+    divBox.push(divEl);
+  }
+  refs.container.append(...divBox);
+  refs.input.value = "";
 }
 
 function destroyBoxes() {
-  refs.input.value = ''
+  refs.input.value = "";
   refs.container.innerHTML = "";
+}
+
+refs.input.addEventListener("keydown", onEnterClick);
+
+function onEnterClick(event) {
+  if (event.code === "Enter") createBoxes();
+  refs.input.addEventListener("keydown", onEscClick);
+}
+
+function onEscClick(event) {
+  if (event.code === "Escape") destroyBoxes();
 }
